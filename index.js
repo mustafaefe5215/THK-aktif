2c65eaf8e9a281c9324aa198a92cd6607554d96a
mustafaefe5215 <mustafaefe5215@gmail.com>
2026-08-29 13:47:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const noblox = require('noblox.js');
 require('dotenv').config();
+const { REST, Routes } = require('discord.js'); require(dotenv').config();
+
+const commands = \[
+
+require('./commands/rutbe-ver').data.toJSON(),
+
+\];const commands = \[
+
+require('./commands/rutbe-ver').data.toJSON(),
+
+\];
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD\_TOKEN);
+
+(async () =\> {
+
+try {
+
+console.log('Slash komutları yükleniyor...'); await rest.put(
+
+Routes.applicationCommands(process.env.CLIENT\_ID), { body: commands },
+
+console.
+
+} catch
+
+console.
+
+}
+
+})0;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
